e00ec648a725561a7148543504c4b83308ccb944
Rafael Perger <rafael.perger@gmail.com>
2022-04-06 18:05:59 +0200

    Fill up header with information. Add evetn listener to search button.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import './style/header.css';
-import { getWeatherData } from './scripts/data';
+import { getWeatherData, processData } from './scripts/data';
+import { displayHeaderDate, fillUpCountrySelector } from './scripts/dom';
 
-getWeatherData('Budapest').then((resp) => console.log(resp));
+const serachBtn = document.querySelector('.search-btn');
+const cityInput = document.querySelector('#city');
+const countryInput = document.querySelector('#country');
+
+displayHeaderDate();
+fillUpCountrySelector();
+serachBtn.addEventListener('click', () =>
+  console.log(cityInput.value, countryInput.value)
+);
+
+// getWeatherData('Budapest').then((resp) =>
+//   console.log(processData(resp.daily[0]))
+// );
